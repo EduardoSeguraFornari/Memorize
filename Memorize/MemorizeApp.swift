24a4257f66f50cad4f201142f878e90bdf98c592
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2022-04-06 15:21:18 -0300

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eduardo Fornari on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
